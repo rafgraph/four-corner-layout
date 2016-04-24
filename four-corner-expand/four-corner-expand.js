e8ee7c8cca9644a49026b0a6c5ef540d0812151e
rafrex <code@rafrex.com>
2016-04-23 20:42:02 -0700

    add original layout w/ html and css only

---
+++ four-corner-expand/four-corner-expand.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('.expand').click(function(e){
+		$('.square').css({"height": "100vh", "width": "100vw"});
+		$('body').css({"height": "200vh", "width": "200vw"})
+	})
+})
