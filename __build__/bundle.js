b56801e3f506ff7dabe683caf6810999089de8f2
rafrex <code@rafrex.com>
2016-06-20 01:57:26 -0700

    fix sticky hover with current-input styles

--- __build__/bundle.js
+++ __build__/bundle.js
@@ -54,19 +54,21 @@
 
 	var _reactRouter = __webpack_require__(168);
 
-	var _App = __webpack_require__(229);
+	__webpack_require__(229);
+
+	var _App = __webpack_require__(230);
 
 	var _App2 = _interopRequireDefault(_App);
 
-	var _Home = __webpack_require__(230);
+	var _Home = __webpack_require__(231);
 
 	var _Home2 = _interopRequireDefault(_Home);
 
-	var _FourCornerScroll = __webpack_require__(234);
+	var _FourCornerScroll = __webpack_require__(235);
 
 	var _FourCornerScroll2 = _interopRequireDefault(_FourCornerScroll);
 
-	var _FourCornerExpand = __webpack_require__(235);
+	var _FourCornerExpand = __webpack_require__(236);
 
 	var _FourCornerExpand2 = _interopRequireDefault(_FourCornerExpand);
 
@@ -25907,6 +25909,146 @@
 
 /***/ },
 /* 229 */
+/***/ function(module, exports) {
+
+	(function() {
+	  'use-strict';
+
+	  var body = document.querySelector('body');
+	  var html = document.querySelector('html');
+	  var currentInput = 'none';
+	  var touchState = false;
+	  var touchTime = new Date();
+
+	  var touchEventsApi = 'ontouchstart' in window;
+	  var anyHover = window.matchMedia("(any-hover: hover)").matches;
+	  var anyPointerFine = window.matchMedia("(any-pointer: fine)").matches;
+	  var pointerEvents = (function() {
+	    if ('PointerEvent' in window) return 'PointerEvent';
+	    if ('MSPointerEvent' in window) return 'MSPointerEvent';
+	  }());
+	  var maxTouchPoints = (function() {
+	    if (window.navigator.maxTouchPoints > 0) return 'maxTouchPoints';
+	    if (window.navigator.msMaxTouchPoints > 0) return 'msMaxTouchPoints';
+	  }());
+
+
+	  // set up based on device type
+	  if (touchEventsApi && (anyHover || anyPointerFine)) {
+	    // hybrid device with touch events api
+	    setTouchMouseEventListeners();
+	    setInitialCurrentInput();
+	  } else if (touchEventsApi || (pointerEvents && maxTouchPoints &&
+	    (anyHover === false && anyPointerFine === false))) {
+	    // required for active state to be enabled when touching the screen
+	    addBlankTouchListenerToBody();
+	    // touch only device, don't set listeners
+	    setCurrentInputClass('touch');
+	  } else if (pointerEvents && maxTouchPoints){
+	    // hybrid device with pointer events api
+	    setPointerEventListeners();
+	    setInitialCurrentInput();
+	  } else {
+	    // mouse only device, don't set listeners
+	    setCurrentInputClass('mouse');
+	  }
+
+
+	  // set current input functions
+	  function setCurrentInputClass(input) {
+	    body.classList.remove('current-input-' + currentInput);
+	    body.classList.add('current-input-' + input);
+	  }
+
+	  function checkInputChange(input) {
+	    if (input !== currentInput) {
+	      setCurrentInputClass(input);
+	      currentInput = input;
+	    }
+	  }
+
+	  function setInitialCurrentInput() {
+	    // set initial current input based on primary input;
+	    if (window.matchMedia("(hover: hover)").matches) {
+	      checkInputChange('mouse');
+	    } else {
+	      checkInputChange('touch');
+	    }
+	  }
+
+
+	  // touch and mouse event functions
+	  function handleTouchStart() {
+	    touchState = true;
+	    checkInputChange('touch');
+	  }
+
+	  function handleTouchEndCancel() {
+	    touchState = false;
+	    touchTime = new Date();
+	  }
+
+	  function handleMouseMoveEnterDown() {
+	    // set state syncronously, no setTimeouts
+	    // currentInput !== 'mouse' is redundent with checkInputChange('mouse'),
+	    // but it short circuts the if clause without calling new Date()
+	    // on every mousemove event.
+	    if (!touchState && currentInput !== 'mouse' &&
+	      new Date() - touchTime > 500) {
+	        checkInputChange('mouse');
+	      }
+	  }
+
+	  function setTouchMouseEventListeners() {
+	    html.addEventListener('touchstart', handleTouchStart, true);
+	    html.addEventListener('touchend', handleTouchEndCancel, true);
+	    html.addEventListener('touchcancel', handleTouchEndCancel, true);
+	    html.addEventListener('mouseenter', handleMouseMoveEnterDown, true);
+	    html.addEventListener('mousedown', handleMouseMoveEnterDown, true);
+	    html.addEventListener('mousemove', handleMouseMoveEnterDown, true);
+	    // have to use mousemove listener because on hybrid chrome
+	    // touch events cause the mouse to disappear (good), and it doesn't
+	    // reappear until it's moved (good), but it only fires mousemove
+	    // events without firing a mouseenter event, despite re-entering the screen.
+	    // Note that MS Edge fires a pointerenter event when the mouse reappears,
+	    // so there is no need to set a pointermove listener.
+	  }
+
+	  function addBlankTouchListenerToBody() {
+	    html.addEventListener('touchstart', function(){}, false);
+	  }
+
+
+	  // pointer event functions
+	  function handlePointerEvent(e) {
+	    var rawType = e.pointerType;
+	    if (
+	      rawType === 'touch' ||
+	      rawType === 2 ||
+	      rawType === 'pen' ||
+	      rawType === 3
+	    ) {
+	      checkInputChange('touch');
+	    } else if (rawType === 'mouse' || rawType === 4) {
+	      checkInputChange('mouse');
+	    }
+	  }
+
+	  function setPointerEventListeners() {
+	    if (pointerEvents === 'PointerEvent') {
+	      html.addEventListener('pointerdown', handlePointerEvent, true);
+	      html.addEventListener('pointerenter', handlePointerEvent, true);
+	    } else if (pointerEvents === 'MSPointerEvent') {
+	      html.addEventListener('MSPointerDown', handlePointerEvent, true);
+	      html.addEventListener('MSPointerEnter', handlePointerEvent, true);
+	    }
+	  }
+
+	}());
+
+
+/***/ },
+/* 230 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -25932,7 +26074,7 @@
 	exports.default = App;
 
 /***/ },
-/* 230 */
+/* 231 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -25947,11 +26089,11 @@
 
 	var _reactRouter = __webpack_require__(168);
 
-	var _ArrowTest = __webpack_require__(231);
+	var _ArrowTest = __webpack_require__(232);
 
 	var _ArrowTest2 = _interopRequireDefault(_ArrowTest);
 
-	var _Corner = __webpack_require__(233);
+	var _Corner = __webpack_require__(234);
 
 	var _Corner2 = _interopRequireDefault(_Corner);
 
@@ -26027,7 +26169,7 @@
 	exports.default = Home;
 
 /***/ },
-/* 231 */
+/* 232 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -26040,7 +26182,7 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
-	var _SvgArrows = __webpack_require__(232);
+	var _SvgArrows = __webpack_require__(233);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -26170,7 +26312,7 @@
 	exports.default = ArrowTest;
 
 /***/ },
-/* 232 */
+/* 233 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -26459,7 +26601,7 @@
 	exports.BottomRightArrow = BottomRightArrow;
 
 /***/ },
-/* 233 */
+/* 234 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -26478,7 +26620,7 @@
 
 	var _reactRouter = __webpack_require__(168);
 
-	var _SvgArrows = __webpack_require__(232);
+	var _SvgArrows = __webpack_require__(233);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -26599,7 +26741,7 @@
 	exports.default = Corner;
 
 /***/ },
-/* 234 */
+/* 235 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -26612,7 +26754,7 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
-	var _Corner = __webpack_require__(233);
+	var _Corner = __webpack_require__(234);
 
 	var _Corner2 = _interopRequireDefault(_Corner);
 
@@ -26652,7 +26794,7 @@
 	exports.default = FourCornerScroll;
 
 /***/ },
-/* 235 */
+/* 236 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
